b65037e5278bb353a5a10bcca42899b641cc8483
TrungBui <bui23@purdue.edu>
2023-02-08 20:09:29 -0500

    done

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Trung Bui on 2/8/23.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
